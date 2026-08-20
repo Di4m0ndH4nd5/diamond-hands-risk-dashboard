1e3506bba122131d8412d640297a6bb37cdbae0e
Di4m0ndH4nd5 <mattnugent97@hotmail.co.uk>
2026-08-20 17:04:35 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='diamond-hands-v1';
+const FILES=['./','index.html','manifest.webmanifest','icon-180.png','icon-192.png','icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(FILES))));
+self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).catch(()=>caches.match(e.request))));
